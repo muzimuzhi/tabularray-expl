ff0e71c6dd4d9d03446f78aab873d9594e7668d8
Yukai Chou <muzimuzhi@gmail.com>
2024-11-19 09:59:26 +0800

    test: reword comments

--- tabularray/build.lua
+++ tabularray/build.lua
@@ -6,7 +6,7 @@ checkdeps       = { maindir .. "/zutil"}
 
 sourcefiles     = { "tabularray.sty" }
 
--- "checkengines" and "checksuppfiles" are overwritten in "config-old.lua"
+-- "checkengines" and "checksuppfiles" will be overwritten in "config-old.lua"
 checkengines    = { "pdftex", "xetex", "luatex" }
 stdengine       = "pdftex"
 checkruns       = 2

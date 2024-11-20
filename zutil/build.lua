068b9da06acb99535145a28ebaae080cf43f47f6
Yukai Chou <muzimuzhi@gmail.com>
2024-11-20 08:06:14 +0800

    wip, new keyval sytnax

--- zutil/build.lua
+++ zutil/build.lua
@@ -3,7 +3,11 @@ module = "zutil"
 
 maindir         = ".."
 
-sourcefiles     = { "zutil.sty" }
+sourcefiles     =
+  {
+    "zutil.sty"
+  , "zutil-debug.code.tex"
+  }
 
 checkengines    = { "pdftex", "xetex", "luatex" }
 stdengine       = "pdftex"

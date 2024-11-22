54e85b7d0d61e9c150501782e13de6fbfde192ec
Yukai Chou <muzimuzhi@gmail.com>
2024-11-23 00:38:29 +0800

    fix(test): make `lvtext` setting in non-default config work
    5290480 (test: restrict `lvtext = ".tex"` to config-old, 2024-11-21)
    silenced config-old tests, due to a l3build problem latex3/l3build#403.
    
    Work around that issue.

--- tabularray/config-old.lua
+++ tabularray/config-old.lua
@@ -5,3 +5,14 @@ checkengines    = { "pdftex" }
 checksuppfiles  = { }
 
 lvtext          = ".tex" 
+
+-- workaround https://github.com/latex3/l3build/issues/403
+test_types = test_types or {} -- needed by ppm checks "texlua buildend.lua"
+test_types.log = {
+  test = lvtext,
+  generated = logext,
+  reference = tlgext,
+  expectation = lveext,
+  compare = compare_tlg,
+  rewrite = rewrite_log,
+}
